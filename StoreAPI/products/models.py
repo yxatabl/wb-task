b2745fd41a0e84bc1a7559f766a5dc327a732eb3
Vladimir <vovagym56@gmail.com>
2026-03-16 14:53:07 +0300

    Added basic structure for products

---
+++ StoreAPI/products/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
