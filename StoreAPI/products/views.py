4f9e980dfce4a38a6d5090d532f2a202fdd6fcbd
Vladimir <vovagym56@gmail.com>
2026-03-16 15:01:03 +0300

    Product methods fixes

--- StoreAPI/products/views.py
+++ StoreAPI/products/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from models import Product
-from serializers import ProductSerializer
+from .models import Product
+from .serializers import ProductSerializer
 
 
 class ProductList(generics.ListCreateAPIView):
