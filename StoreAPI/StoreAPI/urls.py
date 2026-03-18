640887fb44bf004b9b632f72f7086dd41bb8fe8e
Vladimir <vovagym56@gmail.com>
2026-03-18 19:50:45 +0300

    added swagger docs

--- StoreAPI/StoreAPI/urls.py
+++ StoreAPI/StoreAPI/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
     path('api/', include('users.urls')),
     path('api/', include('carts.urls')),
     path('api/', include('orders.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
